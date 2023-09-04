0ab11c5134a6b71b4b1f3547c4a61aab8afb34cf
Frederick B Harway <59441740+Forwarderr@users.noreply.github.com>
2023-09-04 22:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def display_gantt_chart(data):
     data['End'] = data['End Year'].apply(format_year)
 
     chart = alt.Chart(data).mark_bar(size=70).encode(
-        x=alt.X('Start Year:Q', axis=alt.Axis(title='Year', format='.0f', labelFontSize=14)),
+        x=alt.X('Start Year:Q', axis=alt.Axis(title='Year', format='.0f', labelFontSize=12)),
         x2=alt.X2('End Year:Q'),
-        y=alt.Y('Dynasty:N', axis=alt.Axis(title='Dynasty', labelFontSize=14, labelFontWeight=900), sort='x'),  # Sort by Start Year in descending order
+        y=alt.Y('Dynasty:N', axis=alt.Axis(title='Dynasty', labelFontSize=14, labelFontWeight=600), sort='x'),  # Sort by Start Year in descending order
         color=alt.Color('Dynasty:N', legend=None),
         tooltip=['Dynasty:N', 'King:N', 'Beginning:N', 'End:N', 'Capital:N', 'Successor:N', 'Achievements:N',
                  'Events:N', 'Occupied Zone:N', 'Wars:N', 'Rituals:N', 'Malpractices:N', 'Employment:N', 'Diets:N']
