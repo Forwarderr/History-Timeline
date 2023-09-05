ed760506aab004524a35cf7d345117afdc35004f
Frederick B Harway <59441740+Forwarderr@users.noreply.github.com>
2023-09-06 01:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,6 +125,7 @@ def display_table(data):
     # Update layout for better appearance
     fig.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),  # Adjust margins
+        height = 600
     )
     return fig
 
